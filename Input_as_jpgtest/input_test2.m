81c6e63d2accf232793e7638216208b2b39d5d9b
Mohammad Faiz Abid <121609515+atfaizzz@users.noreply.github.com>
2024-11-30 18:43:20 +0530

    #Using Gaussian Kernel to implement FT

--- Input_as_jpgtest/input_test2.m
+++ Input_as_jpgtest/input_test2.m
@@ -2,43 +2,28 @@ input_image = imread('image_002.jpg');
 if size(input_image, 3) == 3
     input_image = rgb2gray(input_image);
 end
-
-% Define the size of the Gaussian kernel and sigma
-kernel_size = 5; % Example size
-sigma = 1.0; % Example sigma
-
-% Create a Gaussian kernel
+kernel_size = 5; 
+sigma = 1.0; 
 [x, y] = meshgrid(-floor(kernel_size/2):floor(kernel_size/2), -floor(kernel_size/2):floor(kernel_size/2));
 gaussian_kernel = exp(-(x.^2 + y.^2) / (2 * sigma^2));
 gaussian_kernel = gaussian_kernel / sum(gaussian_kernel(:));
-
-% Perform convolution
 convolved_image = conv2(double(input_image), gaussian_kernel, 'same');
-
-% Apply Fourier Transform
 fourier_transform = fftshift(fft2(double(input_image)));
 magnitude_spectrum = log(1 + abs(fourier_transform));
-
-% Display matrix sizes
 disp('Input Image Size (rows x columns):');
 disp(size(input_image));
 disp('Output Image Size (rows x columns):');
 disp(size(convolved_image));
-
-% Display results
 figure;
 subplot(2, 2, 1);
 imshow(input_image, []);
 title('Input Image');
-
 subplot(2, 2, 2);
 imshow(gaussian_kernel, []);
 title('Gaussian Kernel');
-
 subplot(2, 2, 3);
 imshow(convolved_image, []);
 title('Convolved Image');
-
 subplot(2, 2, 4);
 imshow(magnitude_spectrum, []);
 title('Fourier Transform Magnitude');
